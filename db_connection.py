a65ba74de1a21392806492d9d3a10fe049fa9e48
ade781 <110089945+ade781@users.noreply.github.com>
2024-11-16 11:19:19 +0700

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -5,9 +5,9 @@ import mysql.connector
 
 def connect_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="kripto",
+        host="sql12.freesqldatabase.com",
+        user="sql12745170",
+        password="eYApSiTED4",
+        database="sql12745170",
         port=3306
     )
